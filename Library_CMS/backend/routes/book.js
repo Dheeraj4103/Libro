994d321e1dec8e4522fa61fc20862bb10d57a554
Dheeraj4103 <dheerajjadhav4455@gmail.com>
2024-03-05 21:02:42 +0530

    cms updated

--- Library_CMS/backend/routes/book.js
+++ Library_CMS/backend/routes/book.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { addBook } = require("../controllers/bookController");
+const { addBook, getAllBooks } = require("../controllers/bookController");
 const router = express.Router();
 
 
 router.post("/add", addBook);
+router.get("/get", getAllBooks)
 
 module.exports = router;
